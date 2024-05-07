1108969d1fcc555733a383f0e80979f22a60ac81
Krenar Hazari <krenarhazari@gmail.com>
2024-05-07 08:14:29 +0200

    created a Slot component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import Slot from "./components/Slot";
 
 function App() {
-  return <div></div>;
+  const possibleValues = ["A", "B", "C"];
+  return (
+    <div>
+      <Slot value={possibleValues.sort(() => Math.random() - 0.5)[0]} />
+    </div>
+  );
 }
 export default App;
---
+++ src/components/Slot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Slot = ({ value, spinning }) => {
+  return (
+    <div className="relative border-4 border-black w-40 h-40 flex items-center justify-center bg-gray-200 ">
+      <div className=" w-full h-full flex items-center justify-center">
+        <span className="text-7xl self-center font-bold">
+          {spinning ? "O" : value}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Slot;
