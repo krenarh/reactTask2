9ab4e3383844bc2c27df8317fca570f8e8ef24c6
Krenar Hazari <krenarhazari@gmail.com>
2024-05-07 09:16:51 +0200

    Added button and slots, made them functionable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Slot from "./components/Slot";
 
 function App() {
+  const [spinning, setSpinning] = useState(false);
+  const [slotValues, setSlotValues] = useState(["O", "O", "O"]);
   const possibleValues = ["A", "B", "C"];
+
+  const spinSlots = () => {
+    if (!spinning) {
+      setSpinning(true);
+      setTimeout(() => {
+        const newValues = possibleValues.map(() => {
+          const randomIndex = Math.floor(Math.random() * possibleValues.length);
+          return possibleValues[randomIndex];
+        });
+        setSlotValues(newValues);
+        setSpinning(false);
+      }, 2000);
+    }
+  };
   return (
     <div>
-      <Slot value={possibleValues.sort(() => Math.random() - 0.5)[0]} />
+      <div className="flex justify-center">
+        {slotValues.map((value, index) => (
+          <Slot key={index} value={value} spinning={spinning} />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <button
+          className="border-2 border-black w-25 h-10 flex items-center justify-center m-1 p-3 rounded-full bg-blue-200 hover:bg-blue-400 font-bold"
+          onClick={spinSlots}
+          disabled={spinning}
+        >
+          {spinning ? "Spinning..." : "Spin"}
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Slot.js
+++ src/components/Slot.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Slot = ({ value, spinning }) => {
   return (
-    <div className="relative border-4 border-black w-40 h-40 flex items-center justify-center bg-gray-200 ">
+    <div className="relative border-4 border-black w-40 h-40 flex items-center justify-center bg-gray-200 m-3">
       <div className=" w-full h-full flex items-center justify-center">
         <span className="text-7xl self-center font-bold">
           {spinning ? "O" : value}
